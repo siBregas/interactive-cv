8868b187e49c1cc0ad8d183c1f7f8a03b1ff42de
Praktikum_2 <dmsbregas@gmail.com>
2025-07-12 13:04:39 +0700

    Final preparation for Vercel deployment

--- frontend/src/assets/data.js
+++ frontend/src/assets/data.js
@@ -1,5 +1,5 @@
 export const educationHistory = [
-  { id: 1, period: '2009 - 2015', institution: 'SDN 1 Jebugan'},
+  { id: 1, period: '2009 - 2015', institution: 'SDN 1 Jebugan', major: 'Sekolah Dasar'},
   { id: 2, period: '2016 - 2017', institution: 'PPMI Assalam', major: 'Mts'},
   { id: 3, period: '2018 - 2021', institution: 'SMK Negeri 2 Klaten', major: 'Elektronika' },
   { id: 4, period: '2023 - Sekarang', institution: 'Universitas AMIKOM', major: 'S1 - Teknik Informatika' }
@@ -17,5 +17,23 @@ export const skills = [
 ];
 
 export const projects = [
-  // Add projects data here if needed
+  {
+    id: 1,
+    title: 'Website CRUD',
+    images: ['/image/CRUD1.png', '/image/CRUD2.png', '/image/CRUD3.png'],
+    image: '/image/CRUD1.png',
+    description: 'Web iseng CRUD barang dengan admin panel untuk mengelola data barang. dan user dapat melihat data barang yang tersedia lalu melakukan proses transaksi yang akan mengurangi stock barang.',
+    tech: ['Vue.js', 'Express.js', 'javascript', 'Prisma'],
+    link: 'https://github.com/siBregas/TokoBarang_vue_express',
+    has_slider: true
+  },
+  {
+    id: 2,
+    title: 'project kelas online',
+    image: 'https://via.placeholder.com/500x300?text=Proyek+2',
+    description: 'web tembat belajar coding online',
+    tech: ['Laravel', 'php'],
+    link: 'https://github.com/KingEery/FP_PemrogWeb',
+    has_slider: false
+  }
 ];
